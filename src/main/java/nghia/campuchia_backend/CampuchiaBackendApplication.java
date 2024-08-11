7f873f2d534889010f7985414c7adf72c19feb71
Matt Nguyen <nguyentrongminhnghia@gmail.com>
2024-08-11 23:04:08 +0700

    first commit

---
+++ src/main/java/nghia/campuchia_backend/CampuchiaBackendApplication.java
@@ -0,0 +1,13 @@
+package nghia.campuchia_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampuchiaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampuchiaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nghia/campuchia_backend/CampuchiaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nghia.campuchia_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampuchiaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
