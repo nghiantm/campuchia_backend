fff0d04a595b9eaab7a8362d4a4c1973e360ac9d
Matt Nguyen <nguyentrongminhnghia@gmail.com>
2024-08-13 23:22:11 +0700

    Added Firebase JWT authentication for all api

---
+++ src/main/java/nghia/campuchia_backend/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package nghia.campuchia_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt());
+
+        return http.build();
+    }
+}
--- src/main/java/nghia/campuchia_backend/controller/HelloController.java
+++ src/main/java/nghia/campuchia_backend/controller/HelloController.java
@@ -12,8 +12,13 @@ public class HelloController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @GetMapping("/hello")
+    @GetMapping("/api/public/hello")
     public Hello greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Hello(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @GetMapping("/api/hello")
+    public Hello private_greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Hello(counter.incrementAndGet(), String.format(template, name));
     }
 }
