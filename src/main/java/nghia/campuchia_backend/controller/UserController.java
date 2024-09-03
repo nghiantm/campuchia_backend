c92e8ce69681521240e2f2a1cc782dccfac7c95f
Matt Nguyen <nguyentrongminhnghia@gmail.com>
2024-09-04 00:03:03 +0700

    Moved auth controller methods into user controller.

--- src/main/java/nghia/campuchia_backend/controller/UserController.java
+++ src/main/java/nghia/campuchia_backend/controller/UserController.java
@@ -7,6 +7,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import nghia.campuchia_backend.dto.ProfileDTO;
+import nghia.campuchia_backend.dto.ProfileWithTokensDTO;
+import nghia.campuchia_backend.dto.SignInCredentialDTO;
 import nghia.campuchia_backend.model.User;
 import nghia.campuchia_backend.service.UserService;
 import nghia.campuchia_backend.utility.JwtUtil;
@@ -17,6 +19,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/user")
 public class UserController {
 
     @Autowired
@@ -25,7 +28,7 @@ public class UserController {
     @Autowired
     private JwtUtil jwtUtil;
 
-    @GetMapping("/api/user")
+    @GetMapping
     @Operation(
             summary = "Get an user by accessToken"
     )
@@ -81,7 +84,7 @@ public class UserController {
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-
+    /*
     @PutMapping("/api/user")
     @Operation(
             summary = "Update user name",
@@ -134,5 +137,90 @@ public class UserController {
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+    } */
+
+    @PostMapping("/signup")
+    @Operation(
+            summary = "Register an user"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "User registered successfully",
+                    content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = ProfileWithTokensDTO.class))
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "409",
+                    description = "Username/email already exist",
+                    content = {
+                            @Content(mediaType = "application/json", schema = @Schema(example = "{\"error\": \"Username/Email already exists.\"}"))
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal server issue. Example: syntax issue, duplicate field in MySQL.",
+                    content = {
+                            @Content(mediaType = "none")
+                    }
+            )
+    })
+    public ResponseEntity<ProfileWithTokensDTO> signUp(@RequestBody User user) {
+        User createdUser = userService.registerNewUser(user);
+        // userService would throw 409 error if duplicate credentials (Email, username)
+        return new ResponseEntity<>(
+                new ProfileWithTokensDTO(
+                        createdUser.getUsername(),
+                        createdUser.getName(),
+                        createdUser.getEmail(),
+                        jwtUtil.generateAccessToken(createdUser.getUsername()),
+                        jwtUtil.generateRefreshToken(createdUser.getUsername())
+                ),
+                HttpStatus.CREATED
+        );
+    }
+
+    @PostMapping("/signin")
+    @Operation(
+            summary = "Sign in an user"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "User signed in successfully",
+                    content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = ProfileWithTokensDTO.class))
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Invalid credentials",
+                    content = {
+                            @Content(mediaType = "application/json", schema = @Schema(example = "{\"error\": \"Invalid credentials.\"}"))
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal server issue. Example: syntax issue, duplicate field in MySQL.",
+                    content = {
+                            @Content(mediaType = "none")
+                    }
+            )
+    })
+    public ResponseEntity<ProfileWithTokensDTO> signIn(@RequestBody SignInCredentialDTO credential) {
+        // userService would throw 401 error if invalid credentials
+        User retrievedUser = userService.validateUserCredentials(credential.getEmail(), credential.getPassword());
+
+        return new ResponseEntity<>(
+                new ProfileWithTokensDTO(
+                        retrievedUser.getUsername(),
+                        retrievedUser.getName(),
+                        retrievedUser.getEmail(),
+                        jwtUtil.generateAccessToken(retrievedUser.getUsername()),
+                        jwtUtil.generateRefreshToken(retrievedUser.getUsername())
+                ),
+                HttpStatus.OK
+        );
     }
 }
